907be765f5469864e2ff29f4c34b46e162104e4d
oliverkean <oliverpalgue@gmail.com>
2025-07-01 10:39:01 +0800

    feat: reduced border

--- src/sections/Project/components/ProjectCards.jsx
+++ src/sections/Project/components/ProjectCards.jsx
@@ -1,7 +1,7 @@
 export default function ProjectCard({ index, name, image, description }) {
   return (
     <li key={index} className="bg-white rounded-xl shadow-md border border-gray-200 p-4 flex flex-col">
-      <img src={image} alt={`${name} logo`} className="w-full rounded-md border-2 border-gray-400 h-56 mb-4 self-center" />
+      <img src={image} alt={`${name} logo`} className="w-full rounded-md border-gray-400 h-56 mb-4 self-center" />
       <h3 className="text-lg font-semibold text-purple-800 mb-2">{name}</h3>
       <p className="text-gray-600 text-sm mb-4">{description}</p>
     </li>
