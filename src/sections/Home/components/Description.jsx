d96c60d0d72178d70a80e9e8f2ef412da41c76f3
oliverkean <oliverpalgue@gmail.com>
2025-07-01 14:34:41 +0800

    feat: added resume document

--- src/sections/Home/components/Description.jsx
+++ src/sections/Home/components/Description.jsx
@@ -1,5 +1,5 @@
 import ScrollButton from '../../../components/ScrollButton'
-
+import resume from '../../../../resume.pdf'
 export default function Description({name}){
     return (
         <div className="w-1/2 text-justify">
@@ -17,7 +17,7 @@ export default function Description({name}){
           </p>
           
           <div className="flex justify-end m-2">
-             <ScrollButton name="View Resume" link=""/>
+             <ScrollButton name="View Resume" link={resume}/>
           </div>
         </div>
     )
