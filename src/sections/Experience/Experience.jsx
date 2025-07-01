4dd278e6f36a2bafdf1fab53df1dbee77388c59a
oliverkean <oliverpalgue@gmail.com>
2025-07-01 20:39:11 +0800

    feat: added responsive for experience

--- src/sections/Experience/Experience.jsx
+++ src/sections/Experience/Experience.jsx
@@ -6,38 +6,42 @@ export default function Experience() {
   return (
     <section
       id="experiences"
-      className="w-full h-full px-4 py-12 shadow-md bg-gray-50"
+      className="w-full h-full px-4 py-12 bg-gray-50 dark:bg-gray-900 shadow-md"
     >
       <div className="max-w-7xl mx-auto">
-        <div className="text-center mb-10 mt-15">
-          <h1 className="text-2xl font-bold text-purple-900">My Experiences</h1>
+        <div className="text-center mb-10">
+          <h1 className="text-2xl font-bold text-purple-900 dark:text-purple-400">
+            My Experiences
+          </h1>
         </div>
-        <div className="flex justify-between gap-2">
-          <ol className="relative border-s border-gray-600">
+        <div className="flex justify-center">
+          <ol className="relative border-s border-gray-400 dark:border-gray-600 w-full px-2 sm:px-6">
             {EXPERIENCE.map(
               ({ company, date, address, role, description }, index) => (
                 <li className="mb-10 ms-6" key={index}>
-                  <span className="absolute flex items-center justify-center w-6 h-6 bg-purple-800 rounded-full -start-3 ring-8 ring-white"></span>
-                  <div className="border border-gray-300 p-4 rounded-md bg-white shadow-sm">
-                    <p className="text-gray-500 text-sm mb-1 flex items-center gap-1.5">
-                      <span><IoCalendarClear /></span>
+                  <span className="absolute flex items-center justify-center w-4 h-4 sm:w-5 sm:h-5 md:w-6 md:h-6 bg-purple-800 dark:bg-purple-500 rounded-full -start-2 sm:-start-2.5 md:-start-3 ring-4 sm:ring-6 md:ring-8 ring-white dark:ring-gray-900"></span>            
+                  <div className="border border-gray-300 dark:border-gray-600 p-4 rounded-md bg-white dark:bg-gray-800 shadow-sm">
+                    <p className="text-gray-500 dark:text-gray-400 text-sm mb-1 flex items-center gap-1.5">
+                      <IoCalendarClear />
                       {date}
                     </p>
-                    <div className="flex justify-between flex-wrap">
-                      <h3 className="text-lg font-bold text-purple-800">
+
+                    <div className="flex justify-between flex-wrap gap-y-2 mb-1">
+                      <h3 className="text-lg font-bold text-purple-800 dark:text-purple-400">
                         {company}
                       </h3>
-                      <span className="text-sm text-gray-500 font-normal flex gap-1.5 items-center">
-                        <span>
-                          <FaLocationDot />
-                        </span>
+                      <span className="text-sm text-gray-500 dark:text-gray-300 flex items-center gap-1.5">
+                        <FaLocationDot />
                         {address}
                       </span>
                     </div>
-                    <p className="mb-1 text-gray-700 text-md font-semibold">
+
+                    <p className="text-md font-semibold text-gray-700 dark:text-gray-200 mb-1">
                       {role}
                     </p>
-                    <p className="text-base text-gray-600">{description}</p>
+                    <p className="text-base text-gray-600 dark:text-gray-300">
+                      {description}
+                    </p>
                   </div>
                 </li>
               )
