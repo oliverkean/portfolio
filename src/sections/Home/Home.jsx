84ad99c381e8c97d4153f44f2e8987c4eca80ed6
oliverkean <oliverpalgue@gmail.com>
2025-07-01 13:18:44 +0800

    feat: added margin top to home

--- src/sections/Home/Home.jsx
+++ src/sections/Home/Home.jsx
@@ -4,7 +4,7 @@ import Description from "../../sections/Home/components/Description";
 
 export default function Home() {
   return (
-    <section id="" className="w-full h-screen p-18 flex items-center shadow-lg border-b border-gray-200">
+    <section id="" className="w-full p-18 mt-20 flex items-center shadow-lg border-b border-gray-200">
       <div className="flex justify-between gap-2 max-w-7xl mx-auto">
           <Introduction name={CONSTANTS.NAME} role={CONSTANTS.FULL_STACK}/>
           <Description name={CONSTANTS.NAME}/>
