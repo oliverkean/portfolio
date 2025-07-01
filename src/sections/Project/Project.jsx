6565aafa5d1ff101cdf911fa2d9fce826d5d032b
oliverkean <oliverpalgue@gmail.com>
2025-07-01 20:52:31 +0800

    feat: added responsive for projects

--- src/sections/Project/Project.jsx
+++ src/sections/Project/Project.jsx
@@ -1,22 +1,27 @@
 import PROJECTS from "../../constants/projects";
 import ProjectCard from "./components/ProjectCards";
-import Logo from "../../assets/images/Logo.png"
+
 export default function Projects() {
   return (
-    <section id="projects" className="w-full h-full p-18 shadow-sm">
+    <section id="projects" className="w-full h-full px-4 py-16 bg-gray-50 dark:bg-gray-800 shadow-sm">
       <div className="max-w-7xl mx-auto">
-        <div className="text-center mb-10 mt-10">
-        <h1 className="text-2xl font-bold text-purple-900">My Projects</h1>
-      </div>
-      <div className="flex justify-center item-center gap-2">
-        <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-          {PROJECTS.map(
-            ({ name, date, description, image }, index) => (
-              <ProjectCard name={name} date={date} image={image} description={description} key={index} />
-            )
-          )}
-        </ul>
-      </div>
+        <div className="text-center mb-10">
+          <h1 className="text-2xl font-bold text-purple-900 dark:text-purple-400">My Projects</h1>
+        </div>
+        <div className="flex justify-center">
+          <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 w-full">
+            {PROJECTS.map(({ name, date, description, image, technologies }, index) => (
+              <ProjectCard
+                key={index}
+                name={name}
+                date={date}
+                image={image}
+                technologies={technologies}
+                description={description}
+              />
+            ))}
+          </ul>
+        </div>
       </div>
     </section>
   );
--- src/sections/Project/components/ProjectCards.jsx
+++ src/sections/Project/components/ProjectCards.jsx
@@ -1,17 +1,24 @@
-export default function ProjectCard({ index, name, image, description }) {
+export default function ProjectCard({ name, image, description, technologies }) {
   return (
-    <li
-      key={index}
-      className="bg-white rounded-xl shadow-md border border-gray-200 flex flex-col transition-transform duration-300 hover:scale-105 hover:shadow-lg hover:border-purple-400"
-    >
+    <li className="bg-white dark:bg-gray-800 rounded-xl shadow-md border border-gray-200 dark:border-gray-600 flex flex-col transition-transform duration-300 hover:scale-105 hover:shadow-lg hover:border-purple-400 dark:hover:border-purple-500">
       <img
         src={image}
-        alt={`${name} logo`}
-        className="w-full rounded-t-xl border-gray-400 h-56 mb-4 object-cover"
+        alt={`${name} project preview`}
+        className="w-full h-56 object-cover rounded-t-xl border-b border-gray-200 dark:border-gray-700"
       />
-      <div className="p-4">
-        <h3 className="text-lg font-semibold text-purple-800 mb-2">{name}</h3>
-        <p className="text-gray-600 text-sm mb-4">{description}</p>
+      <div className="p-4 flex flex-col flex-grow">
+        <h3 className="text-lg font-semibold text-purple-800 dark:text-purple-300 mb-2">{name}</h3>
+        <p className="text-sm text-gray-600 dark:text-gray-300 mb-4 flex-grow">{description}</p>
+        <div className="flex flex-wrap gap-2 mb-2">
+          {technologies.map((tech, index) => (
+            <span
+              key={index}
+              className="flex items-center justify-center text-xs bg-purple-100 dark:bg-purple-700 text-purple-800 dark:text-white font-medium px-2 py-0.5 rounded-full"
+            >
+              {tech}
+            </span>
+          ))}
+        </div>
       </div>
     </li>
   );
