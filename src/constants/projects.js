53e39898b7c7ceb62928be3360d7849e0ca10e93
oliverkean <oliverpalgue@gmail.com>
2025-07-01 09:43:34 +0800

    feat: added description to projects

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -1,19 +1,19 @@
 export const PROJECTS = [
     {
         name: 'Realtime Traffic Monitoring System',
-        description: 'Developed a real-time traffic monitoring system with Laravel and Vue.js, using third-party APIs and Laravel Reverb for live updates and a seamless UI. Created technical documentation to support scalability and team collaboration.'
+        description: 'A web application to help users track the status of traffic within the roads at Baguio City. Traffic is one of the issues within the city, this web application will be used to help users decide alternative routes to reach their destinations. This will be managed by admins by updating the traffic conditions within each route in real time. '
     },
     {
         name: 'Swapii: Bartering Web Application',
-        description: 'Completed cross-functional training in QA, HR, and design, enhancing adaptability and understanding of product workflows. Contributed to QA by identifying bugs and usability issues, and supported team collaboration through regular progress updates.'
+        description: 'A web-based platform developed using the VILT stack (Vue.js, Inertia.js, Laravel, Tailwind CSS) that enables University of Baguio students and staff to barter items to reduce waste and help students financially. Features include real-time chat, trade management, user authentication, AI image validation, review/rating system, and admin analytics for monitoring activity and managing user.'
     },
     {
         name: 'Inventory System for Poultry Business',
-        description: 'Built two scalable full-stack apps (poultry MIS and bartering platform) with Laravel and Vue.js, featuring responsive UIs and key modules like inventory tracking and reporting. Collaborated in an agile team, using GitHub and user testing to ensure functionality and usability.'
+        description: 'A web-based system built with Laravel to manage poultry inventory, suppliers, and customer orders. It offers real-time updates, user authentication, and report generation about business operations and improve accuracy.'
     },
     {
         name: 'TravelCations: Travel Website for Baguio City',
-        description: 'Built two scalable full-stack apps (poultry MIS and bartering platform) with Laravel and Vue.js, featuring responsive UIs and key modules like inventory tracking and reporting. Collaborated in an agile team, using GitHub and user testing to ensure functionality and usability.'
+        description: 'TravelCations is a tourism-focused web platform designed to showcase popular destinations, and local experiences in Baguio City. It provides users with travel guides. The site aims to boost local tourism through accessible and informative content.'
     },
 ]
 
--- src/sections/Project/components/ProjectCards.jsx
+++ src/sections/Project/components/ProjectCards.jsx
@@ -2,9 +2,7 @@ export default function ProjectCard({ index, name, img, date, description }) {
   return (
     <li key={index} className="bg-white rounded-xl shadow-md border border-gray-200 p-4 flex flex-col">
       <img src={img} alt={`${name} logo`} className="w-16 h-16 mb-4 self-center" />
-      <div className="mb-2">
-        <h3 className="text-lg font-semibold text-purple-800">{name}</h3>
-      </div>
+      <h3 className="text-lg font-semibold text-purple-800 mb-2">{name}</h3>
       <p className="text-gray-600 text-sm">{description}</p>
     </li>
   );
