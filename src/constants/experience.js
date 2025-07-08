ba6e41acca9a977c09d7b5c5381abf807832551e
Oliver Kean L. Palgue <98378847+oliverkean@users.noreply.github.com>
2025-07-08 10:44:20 +0800

    Update experience.js

--- src/constants/experience.js
+++ src/constants/experience.js
@@ -4,22 +4,22 @@ export const EXPERIENCE = [
         date: 'April 2025 - June 2025',
         address: 'Convention Center, Baguio City, Benguet',
         role: 'Full Stack Developer',
-        description: 'Developed a real-time traffic monitoring system with Laravel and Vue.js, using third-party APIs and Laravel Reverb for live updates and a seamless UI. Created technical documentation to support scalability and team collaboration.'
-    },
-    {
-        company: 'Highly Succeed Inc.',
-        date: 'Feb 2025 - April 2025',
-        address: 'Mandaluyong, Metro Manila',
-        role: 'Computer Science Intern',
-        description: 'Completed cross-functional training in QA, HR, and design, enhancing adaptability and understanding of product workflows. Contributed to QA by identifying bugs and usability issues, and supported team collaboration through regular progress updates.'
+        description: 'Developed a real-time traffic monitoring system using Laravel, Vue.js, Mapbox, and WebSockets. Designed a responsive UI with Tailwind CSS, integrated RESTful APIs, implemented secure role-based authentication, and authored technical documentation for maintainability.'
     },
     {
         company: 'University of Baguio',
-        date: 'Aug 2022 - Jan 2025',
+        date: 'Jan 2024 - Dec 2024',
         address: 'Gen. Luna Rd, Baguio City',
-        role: 'Computer Science Student',
-        description: 'Built two scalable full-stack apps (poultry MIS and bartering platform) with Laravel and Vue.js, featuring responsive UIs and key modules like inventory tracking and reporting. Collaborated in an agile team, using GitHub and user testing to ensure functionality and usability.'
+        role: 'Full Stack Developer – Swapii (E-commerce Bartering Platform)',
+        description: 'Developed a Laravel and Vue (Inertia.js) trading platform with chat, image/video messaging, and real-time updates using Pusher. Deployed and did a User Acceptance Testing with 90+ students, and delivered system documentation including SRS, SDD, and test cases.'
     },
-]
+    {
+        company: 'University of Baguio',
+        date: 'Aug 2023 -  Jan 2024',
+        address: 'Gen. Luna Rd, Baguio City',
+        role: 'Full Stack Developer – Inventory System for Poultry Business',
+        description: 'Built an MIS inventory system using Laravel Livewire with analytics, inventory tracking, and secure user authentication. Designed a Bootstrap-based interface, conducted stakeholder interviews for real-world modeling, and created full system documentation.'
+    },
+];
 
 export default EXPERIENCE;
